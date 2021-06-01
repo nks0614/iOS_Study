56a01a9844cede55e20ca6e2dc54a716d9293652
nks0614 <namgyeseok0614@naver.com>
2021-06-01 10:28:03 +0900

    [ADD] Button
    Add button and click button alert function

--- NKS_Study/ViewController.swift
+++ NKS_Study/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showAlert() {
+        // add alert
+        let alert = UIAlertController(title: "hello", message : "MY FIRST APP", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showSecondAlert() {
+        let alert = UIAlertController(title: "2nd", message: "YOU CLICK SECOND", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 
 }
